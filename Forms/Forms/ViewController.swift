045654e2166c8d8482d1edbb539bdf15a457afb1
Isaac Watt <isaacwatt@icloud.com>
2019-02-07 21:46:23 -0500

    Forms project init

---
+++ Forms/Forms/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Forms
+//
+//  Created by Isaac Watt on 2019-02-07.
+//  Copyright © 2019 Isaac Watt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
