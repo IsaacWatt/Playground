d9637a153e030f125d56ae6e0e341123a700f26a
Isaac Watt <isaacwatt@icloud.com>
2019-02-24 13:52:37 -0500

    init stack view project

---
+++ stackview/stackview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stackview
+//
+//  Created by Isaac Watt on 2019-02-24.
+//  Copyright © 2019 Isaac Watt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
